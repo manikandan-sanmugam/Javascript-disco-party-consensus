b1ff20261b5a15d736fdb165c8d72455074fa404
Manikandan S <56933033+manikandan-sanmugam@users.noreply.github.com>
2023-07-24 17:43:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let intertvalId;
+document.getElementById("h1").addEventListener('click',() => {
+   intervalId = setInterval(func,750)
+});
+
+ function func() {
+    if(true){
+const rand =Math.floor(Math.random() * 4);
+    
+   if(rand==0){
+    document.getElementById("h1").innerHTML = "ON THE FLOOR BABY";
+    const head = document.querySelector('body');
+    head.className = "bg1";
+   }
+   else if(rand==1){
+    document.getElementById("h1").innerHTML = "HIT IT HARD BABY";
+    const head = document.querySelector('#body');
+
+    
+    head.className = "bg2";
+
+   }
+   else if(rand ==3){
+    document.getElementById("h1").innerHTML = "ROCK THE PARTY BABY";
+    const head = document.querySelector('#body');
+
+   
+    head.className = "bg3";
+
+   }
+   else{
+    const head = document.querySelector('#body');
+    head.className = "bg4";
+    document.getElementById("h1").innerHTML = "DANCE";
+ 
+
+   }}
+   }
+
+
+
+
